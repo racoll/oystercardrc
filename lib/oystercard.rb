bf407598cb05b93afedebb1577117d6d4e3f914e
robincollins <racollins1@me.com>
2018-01-03 21:54:32 +0000

    added min balance functionality and deduct capability

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,6 +1,7 @@
 class Oystercard
 
   MAXIMUM_BALANCE = 90
+  MINIMUM_BALANCE = 3
 
   attr_reader :balance
 
@@ -11,6 +12,11 @@ class Oystercard
   def top_up(amount)
     fail "Maximum balance of #{MAXIMUM_BALANCE} exceeded" if amount + balance > MAXIMUM_BALANCE
     @balance += amount
+  end
+
+  def deduct(amount)
+    fail "Insufficient funds. Please top up" if balance - amount < MINIMUM_BALANCE
+    @balance -= amount
   end
 
   # def max_balance
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -8,7 +8,7 @@ describe Oystercard do
     expect(subject.balance).to eq (0)
   end
 
-    describe '#top_up' do
+    describe "#top_up" do
 
       it { is_expected.to respond_to(:top_up).with(1).argument }
 
@@ -28,6 +28,26 @@ describe Oystercard do
       subject.top_up(maximum_balance)
       expect{ subject.top_up 1 }.to raise_error "Maximum balance of #{maximum_balance} exceeded"
     end
+
+
+    describe "#deduct" do
+
+      it "can have a fare deducted from the balance" do
+        oystercard.top_up(10)
+        oystercard.deduct(5)
+        expect(oystercard.balance).to eq (5)
+      end
+
+      it "raises an error if the balance drops below the minimum balance" do
+        minimum_balance = Oystercard::MINIMUM_BALANCE
+        oystercard.top_up(10)
+        oystercard.deduct(7)
+        expect { oystercard.deduct 1}.to raise_error "Insufficient funds. Please top up"
+      end
+
+    end
+
+
 
 
 
