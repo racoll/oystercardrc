934eabad4b461f74bfb15beb7a06a201fe041af0
robincollins <racollins1@me.com>
2018-01-03 21:33:54 +0000

    created method for max balance

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,7 +1,17 @@
 class Oystercard
 
-  def balance
-    0
+  attr_reader :balance
+
+  def initialize
+    @balance = 0
+  end
+
+  def top_up(amount)
+    @balance += amount
+  end
+
+  def max_balance
+    90
   end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,8 +2,32 @@ require "oystercard"
 
 describe Oystercard do
 
+  subject(:oystercard) { described_class.new }
+
   it "has a balance of zero" do
     expect(subject.balance).to eq (0)
   end
+
+    describe '#top_up' do
+
+      it { is_expected.to respond_to(:top_up).with(1).argument }
+
+      # it 'can top up the balance' do
+      #   expect{ subject.top_up 1 }.to change{ subject.balance }.by 1
+      # end
+
+      it "can top up the balance" do
+        oystercard.top_up(1)
+        expect(oystercard.balance).to eq (1)
+      end
+
+    end
+
+  it "has a maximum balance of ninety" do
+    expect(oystercard.max_balance).to eq (90)
+  end
+
+
+
 
 end
